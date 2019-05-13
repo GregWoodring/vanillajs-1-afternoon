e0f4d3068e248aedcf57d2778c2b1bc46fec1412
Windwaker02 <woodringg95@gmail.com>
2019-05-13 15:49:36 -0400

    finished part 2

--- Part2/script.js
+++ Part2/script.js
@@ -1,2 +1,25 @@
 let cardId = document.getElementById('cardId');
 let cardStyle = document.getElementById('cardStyle');
+
+function setCard(){
+    try{
+        let value = cardId.value;
+        if(value !== "spades" &&
+        value !== "hearts" &&
+        value !== "diamonds" &&
+        value !== "clubs") throw Error(`Invalid Id entered: ${value}`)
+
+        let card = document.getElementById(value);
+        card.style.color = cardStyle.value;
+    }
+    catch(err){
+        alert(err);
+    }
+}
+
+function reset(){
+    document.getElementById('spades').style.color = 'grey';
+    document.getElementById('hearts').style.color = 'grey';
+    document.getElementById('clubs').style.color = 'grey';
+    document.getElementById('diamonds').style.color = 'grey';
+}
